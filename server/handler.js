3e83cc66bacf7c2fda61e19f021de79c5b504eb0
heshima18 <122611275+heshima18@users.noreply.github.com>
2023-04-05 17:17:50 +0200

    Update handler.js

--- server/handler.js
+++ server/handler.js
@@ -6,7 +6,7 @@ let connection = mysql.createConnection({
 	host : 'sql108.epizy.com',
 	user : 'epiz_31932252',
 	password : 'mHz5Co3utx',
-	database : 'epiz_31932252_sourcecodester_posales'
+	database : 'epiz_31932252_itzone'
 })
 try {
 	connection.connect(function (error) {
