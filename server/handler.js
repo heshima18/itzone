03dcf9129e5b88f18670a8e34a5ee08e9a9d4a6c
heshima18 <122611275+heshima18@users.noreply.github.com>
2023-04-14 15:47:39 +0200

    Update handler.js

--- server/handler.js
+++ server/handler.js
@@ -3,30 +3,25 @@ let bodyParser = require('body-parser');
 let mysql = require('mysql');
 let app = express();
 let path = require('path');
-
 let connection = mysql.createConnection({
+
 	host : 'sql.freedb.tech',
 	user : 'freedb_itzone',
 	password : 'X6$Xn#JnTEW7z@w',
-	database : 'freedb_itzone',
-	insecureAuth: true,
-  	multipleStatements: true,
-  	max_allowed_packet: 64 * 1024 * 1024,
+	database : 'freedb_itzone'
 })
-let v = ()=> {connection.connect(function (error) {
-	if(error)
-		console.log(error)
-	else
-		console.log('connected to my sql')
-})
-}
-v()
-
-connection.on('error', function(err) {
-  console.error('Database connection error:', err.stack);
-	v()
+try {
+	connection.connect(function (error) {
+		if(error)
+		 console.log(error)
+			else
+				console.log('connected to my sql')
+	})
 	
-});
+} catch (error) {
+	console.log(error)
+}
+
 const server = app.listen(8080,()=>{
 		
 		console.log("app was connected to port 8080");
